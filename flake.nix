976d471436760ee604ac53ea27d5c874dce6a238
taku-271 <ta.yamashita.314@gmail.com>
2026-03-18 23:21:53 +0900

    feat: nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Zenn.dev article";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            hello
+          ];
+        };
+      }
+    );
+}
+
